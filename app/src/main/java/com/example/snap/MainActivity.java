3604dd80fc8186ee96e06bc954e42b4ab5ffac96
Nikita <nikitamittal2423@gmail.com>
2019-09-21 23:01:45 +0530

    Camera Permission not working

--- app/src/main/java/com/example/snap/MainActivity.java
+++ app/src/main/java/com/example/snap/MainActivity.java
@@ -1,13 +1,125 @@
 package com.example.snap;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button captureImage, saveImage;
+    ImageView cameraImageView;
+    private static final int TAKE_PICTURE_REQUEST_B = 100;
+    private Bitmap mCameraBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        captureImage = (Button)findViewById(R.id.capture_image_button);
+        saveImage = (Button)findViewById(R.id.save_image_button);
+        cameraImageView = (ImageView)findViewById(R.id.camera_image_view);
+        captureImage.setOnClickListener(this);
+        saveImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.capture_image_button:
+                startImageCapture();
+                break;
+            case R.id.save_image_button:
+                File saveFile = openFileForImage();
+                if (saveFile != null) {
+                    saveImageToFile(saveFile);
+                } else {
+                    Toast.makeText(MainActivity.this, "Unable to open file for saving image.",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+
+    public void startImageCapture(){
+        startActivityForResult(new Intent(MainActivity.this, CameraActivity.class), TAKE_PICTURE_REQUEST_B);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == TAKE_PICTURE_REQUEST_B){
+            if(resultCode == RESULT_OK){
+                if(mCameraBitmap !=null){
+                    mCameraBitmap.recycle();
+                    mCameraBitmap = null;
+                }
+                Bundle extras = data.getExtras();
+                byte[] cameraData = extras.getByteArray(CameraActivity.EXTRA_CAMERA_DATA);
+                if (cameraData != null) {
+                    mCameraBitmap = BitmapFactory.decodeByteArray(cameraData, 0, cameraData.length);
+                    cameraImageView.setImageBitmap(mCameraBitmap);
+                    //saveImage.setEnabled(true);
+                }
+            } else{
+                mCameraBitmap = null;
+               // saveImage.setEnabled(false);
+            }
+        }
+    }
+
+
+    private File openFileForImage() {
+        File imageDirectory = null;
+        String storageState = Environment.getExternalStorageState();
+        if (storageState.equals(Environment.MEDIA_MOUNTED)) {
+            imageDirectory = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "com.example.snap");
+            if (!imageDirectory.exists() && !imageDirectory.mkdirs()) {
+                imageDirectory = null;
+            } else {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd_hh_mm",
+                        Locale.getDefault());
+
+                return new File(imageDirectory.getPath() +
+                        File.separator + "image_" +
+                        dateFormat.format(new Date()) + ".png");
+            }
+        }
+        return null;
+    }
+
+    private void saveImageToFile(File file) {
+        if (mCameraBitmap != null) {
+            FileOutputStream outStream = null;
+            try {
+                outStream = new FileOutputStream(file);
+                if (!mCameraBitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)) {
+                    Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Saved image to: " + file.getPath(),
+                            Toast.LENGTH_LONG).show();
+                }
+                outStream.close();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
